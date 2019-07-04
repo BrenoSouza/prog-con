5360c727600bb18c19334cec50fafe2681f10fbb
Breno <jose.souza@splab.ufcg.edu.br>
2019-07-03 22:19:23 -0300

    Refatoracao questao1A

--- questao1A/questao1A.go
+++ questao1A/questao1A.go
@@ -12,38 +12,38 @@ import (
 	"time"
 )
 
+const MIN_NUMBER = 1
+const MAX_NUMBER = 30
+
 func main() {
-	number := 5
-	gateway(number)
+
+	num_replicas := 5
+	result := gateway(num_replicas)
+	fmt.Printf("Valor da primeira goroutine: %d", result)
 }
 
 /**
 Funçaõ que inicia num_replicas goroutines e cada thread goroutines a função request
 */
-func gateway(num_replicas int) {
+func gateway(num_replicas int) int {
 	ch := make(chan int)
 
 	for i := 0; i < num_replicas; i++ {
 
 		fmt.Printf("%d goroutine começou\n", i+1)
 
+		// Passei o index da goroutine por motivos de visualização
 		go request(ch, i+1)
 	}
 
-	fmt.Println(<-ch)
-
-	// select {
-	// case req := <-ch:
-	// 	fmt.Println(req)
-	// }
-
+	return <-ch
 }
 
 /**
 Sorteia números aleatórios entre 1 e 30, dormir pelo tempo do número e adiciona no channel o número
 */
 func request(ch chan<- int, index int) {
-	number := generateRandomNumber()
+	number := generateRandomNumber(MIN_NUMBER, MAX_NUMBER)
 	// time.Duration pra converter do tipo int pra duration
 	fmt.Printf("%d goroutine dormindo por %d segundos\n", index, number)
 
@@ -58,15 +58,13 @@ func request(ch chan<- int, index int) {
 Função criada para gerar um número inteiro aleatório entre 1 e 30
 @return inteiro entre 1 e 30
 */
-func generateRandomNumber() int {
+func generateRandomNumber(min int, max int) int {
 	// Se usar só rand de int, ele gera sempre o msm número, pois topLevel functions compartilham um source que gera valores deterministicos
 	// Desse jeito abaixo ele gera um número baseado no tempo em que foi executado
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	rand.Seed(time.Now().UnixNano())
 	// Tem que somar o mínimo, porque por default o valor mínimo gerado é 0
-	min := 1
 	// Será que o intervalo é aberto no limite superior?
-	max := 30
-	number := r.Intn(max-min) + min
+	number := rand.Intn(max-min) + min
 
 	return number
 }
